d662288a09982940ceb4105b5082ab47e32f23f6
cyt228 <amychen920228@gmail.com>
2026-05-05 14:17:44 +0000

    Update run_one

--- run_one.sh
+++ run_one.sh
@@ -10,8 +10,7 @@ SAVE_DIR="/home/code-server/NO7_pred_result/062"
 NAME=$(basename "$VIDEO_FILE")
 NAME="${NAME%.*}"
 
-#CUDA_VISIBLE_DEVICES=2 python predict.py --video_file "$VIDEO_FILE" --tracknet_file exp/TrackNet_best.pt --inpaintnet_file exp/InpaintNet_best.pt --save_dir "$SAVE_DIR" --large_video
->>>>>>> eafb225 (Inference Pipeline 效能優化)
+CUDA_VISIBLE_DEVICES=2 python predict.py --video_file "$VIDEO_FILE" --tracknet_file exp/TrackNet_best.pt --inpaintnet_file exp/InpaintNet_best.pt --save_dir "$SAVE_DIR" --large_video
 python speed_analysis/stroke_zone_analysis.py --video_file "$VIDEO_FILE" --ball_csv "$SAVE_DIR/${NAME}_ball.csv" --save_dir "$SAVE_DIR"
 python speed_analysis/plot_speed.py --input "$SAVE_DIR"
 
